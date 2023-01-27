59767ec2a615147a4c2b6a643e0b42365d56659f
voronovski <avoronovski@gmail.com>
2023-01-26 22:28:33 -0800

    files reorganization

---
+++ RickAndMorty/Models/Character.swift
@@ -0,0 +1,13 @@
+//
+//  Character.swift
+//  RickAndMorty
+//
+//  Created by Aleksei Voronovskii on 1/26/23.
+//
+
+struct Character: Decodable {
+    let id: Int?
+    let name: String?
+    let gender: String?
+    let image: String?
+}
